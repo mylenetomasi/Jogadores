364f836af0c3d9468e4214daeae99388864e8f69
mylenetomasi <99373744+mylenetomasi@users.noreply.github.com>
2022-05-30 09:30:41 -0300

    Add files via upload

---
+++ times-jogadores-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <span class="texto-header">Home</span>
+    <span class="separador"> | </span>
+    <span class="texto-header">Jogadores</span>
+    <span class="separador"> | </span>
+    <span class="texto-header"> Times </span>
+  </header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /* padding: 2rem; */
+  height: 100vh;
+
+  font-weight: normal;
+}
+
+header,
+footer {
+  background-color: rgb(70, 24, 112);
+  height: 15%;
+  font-size: 1.3rem;
+  color: white;
+  display: flex;
+  align-items: center;
+  padding-left: 1.5rem;
+}
+
+main {
+  min-height: 70%;
+}
+
+header span {
+  padding: 0 10px;
+}
+</style>
